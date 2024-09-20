de0dccd5ee29232aa82323b34eb536394cb0f179
Sowrin Paul <paul.sowrin2002@gmail.com>
2024-09-20 09:21:57 +0600

    started editing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
+import { BrowserRouter } from "react-router-dom";
+import { Route } from "react-router-dom";
+import './style';
+import { About } from './components/About';
+import { Navbar } from './components/Navbar';
+import { Hero } from './components/Hero';
+import { Contact } from './components/Contact';
+import { Experience } from './components/Experience';
+import { Works } from './components/Works';
+import { Tech } from './components/Tech';
+import { Feedbacks } from './components/Feedbacks';
+import { StarsCanvas } from './components/canvas/Stars';
+
 const App = () =>{
   return(
-    <>
-      
-    </>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/About.jsx
@@ -0,0 +1,7 @@
+const About = () => {
+    return (
+        <h1>this is About section</h1>
+    );
+}
+
+export default About;
