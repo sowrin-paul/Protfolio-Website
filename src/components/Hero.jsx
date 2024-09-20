4b4b6adbf0c4ce62a2d6e8f22e5d499257b67103
Sowrin Paul <paul.sowrin2002@gmail.com>
2024-09-20 15:11:17 +0600

    added hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,61 @@
+import { motion } from "framer-motion";
+
+import { styles } from "../style";
+import { ComputersCanvas } from "./canvas";
+import { style } from "framer-motion/client";
+
 const Hero = () => {
     return (
-        <h1>this is hero section</h1>
+        <section className="relative w-full h-screen mx-auto">
+            <div className={`${styles.paddingX} absolute inset-0 top-[120px] 
+                                                max-w-7xl mx-auto 
+                                                flex flex-row 
+                                                items-start gap-5`}
+            >
+                <div className="flex flex-col items-center justify-center mt-5">
+                    <div className="w-5 h-5 rounded-full bg-[#02a3eb]" />
+                    <div className="w-1 sm:h-80 h-40 blue-gradient" />
+                </div>
+                <div>
+                    <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span 
+                        className="text-[#02a3eb]">Sowrin</span></h1>
+                    <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+                        I develop web applications, user 
+                        <br className="sm:block hidden"/>inteface, and working on some machine learning applications
+                    </p>
+                </div>
+            </div>
+
+            {/* computer 3d model */}
+            <ComputersCanvas />
+
+            {/* Scroller */}
+            <div
+                className="absolute xs:bottom-10
+                            bottom-32 w-full flex justify-center items-center"
+            >
+                <a href="#about">
+                    <div
+                        className="w-[35px] h-[64px]
+                        rounded-3xl border-4 border-secondary flex
+                        justify-center items-start p-2"
+                    >
+                        <motion.dev 
+                            animate={{
+                                y: [0, 24, 0]
+                            }}
+                            transition={{
+                                duration: 1.5,
+                                repeat: Infinity,
+                                repeatType: 'loop',
+                            }}
+                            className="w-3 h-3 rounded-full bg-secondary mb-1"
+                        />
+                    </div>
+                </a>
+            </div>
+
+        </section>
     );
 }
 
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import { Suspense, useState, useEffect } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 
-const Computers = () => {
-  return (
-    <div>Computers</div>
-  )
+import CanvasLoader from "../Loader";
+
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF('/desktop_pc/scene.gltf');
+
+  return(
+    <mesh>
+      <hemisphereLight intensity={0.5} groundColor="grey" />
+      <pointLight intensity={50} position={[1, 1, 1]} />
+      <spotLight
+        position={[1, 1, 1]}
+        angle={0.3}
+        penumbra={0.5}
+        intensity={2}
+        castShadow
+      />
+      <primitive 
+        object={computer.scene}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ?  [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
+  );
 }
 
-export default Computers
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
+
+      setIsMobile(mediaQuery.matches);
+
+      const handleMediaQueryChange = (event) => {
+        setIsMobile(event.matches);
+      }
+
+      mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+      return () => {
+        mediaQuery.removeEventListener('change' ,handleMediaQueryChange);
+      }
+  }, []);
+
+  return(
+    <Canvas
+      flameLoop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />} >
+        <OrbitControls enableZoom={false} 
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+    </Canvas>
+  );
+}
+
+export default ComputersCanvas;
